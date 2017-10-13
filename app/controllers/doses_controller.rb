0fc7bc336431561adf6e3ead7d1e40af3715b323
junggunlee <leejunggun7@gmail.com>
2017-10-13 18:59:22 +0900

    edit dose controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,21 +2,20 @@ class DosesController < ApplicationController
   before_action :set_dose, only: [:destroy]
 
   def new
-    @dose = Dose.new
+    # @dose = Dose.new
     # @array_ingredients = []
     # @ingredients = Ingredient.all
     # @ingredients.each do |ingredient|
     #   @array_ingredients << ingredient
     # end
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.build
+    # .build is method for active record.
   end
 
   def create
-    @dose = Dose.new(dose_params)
-    # @ingredient = Ingredient.find(params[:ingredient_id])
     @cocktail = Cocktail.find(params[:cocktail_id])
-    # @dose.ingredient = @ingredient
-    # @dose.cocktail = @cocktail
+    @dose = @cocktail.doses.build(dose_params)
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -36,6 +35,6 @@ class DosesController < ApplicationController
   end
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+    params.require(:dose).permit(:ingredient_id, :description)
   end
 end
