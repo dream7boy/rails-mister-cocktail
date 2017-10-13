abd670d5824051d45c91abbe8be1bd18f626a6c4
junggunlee <leejunggun7@gmail.com>
2017-10-13 13:08:52 +0900

    make delete cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
   root "cocktails#index"
-  resources :cocktails, only: [ :index, :new, :show, :create ] do
+  resources :cocktails, only: [ :index, :new, :show, :create, :destroy ] do
     resources :doses, only: [ :new, :create ]
   end
   resources :doses, only: [ :destroy ]
